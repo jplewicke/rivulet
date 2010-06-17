2ef22846a4190e4a59bd837b9f472dbc3b5f8663
John Paul Lewicke <jplewicke@john-paul-lewickes-macbook.local>
2010-06-17 00:11:30 -0400

    Trying Ruby.

--- VIRTENV/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env /Users/jplewicke/rivulet/VIRTENV/bin/jython
-# EASY-INSTALL-SCRIPT: 'Django==1.1.1','django-admin.py'
-__requires__ = 'Django==1.1.1'
-import pkg_resources
-pkg_resources.run_script('Django==1.1.1', 'django-admin.py')
