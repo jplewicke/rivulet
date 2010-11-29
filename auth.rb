11ae951bab70c784112b3fc2914bb8cc70f1e5f2
John Paul Lewicke <jplewicke@john-paul-lewickes-macbook.local>
2010-11-29 13:21:45 -0500

    Interface is almost working.

--- auth.rb
+++ auth.rb
@@ -4,7 +4,7 @@ require "neo_classes"
 #Throws a 401 if the provided credentials don't match at least one of the enumerated users.
 def protected!(user_ids)
   unless user_ids.any? {|user_id| authorized?(user_id) }
-    response['WWW-Authenticate'] = %(Basic realm="Rivulet")
+    response['WWW-Authenticate'] = %(Basic realm="You may have mistyped your username or password. You can either retype them here or hit ESC twice.")
     throw(:halt, [401, "Not authorized\n"])
   end
 end
@@ -12,6 +12,13 @@ end
 def authorized?(user_id)
   user_creds = User.creds_from_id(user_id)
   @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+  puts "user_id: #{user_id}"
+  puts "auth.provided? #{@auth.provided?}"
+  puts "auth.basic? #{@auth.basic?}"
+  puts "auth.credentials[0]: #{@auth.credentials[0]}"
+  puts "auth.credentials[1]: #{@auth.credentials[1]}"
+  puts "user_creds[0]: #{user_creds}"
+  #puts "user_creds[1]: #{user_creds[1]}"
   @auth.provided? && @auth.basic? && @auth.credentials && @auth.credentials == user_creds
 end
 
--- public/actions.js
+++ public/actions.js
@@ -7,15 +7,43 @@ actions["make_reserved_payment"] = new Object;
 actions["request_payment"] = new Object;
 actions["view_credit_balance"] = new Object;
 
-actions["make_credit_offer"]["label"] = "Make Credit Offer"
-actions["accept_credit_offer"]["label"] = "Accept Credit Offer"
-actions["make_payment"]["label"] = "Make Payment"
-actions["make_reserved_payment"]["label"] = "Make Reserved Payment"
-actions["request_payment"]["label"] = "Request Payment"
-actions["view_credit_balance"]["label"] = "View Credit Balance";
+actions["make_credit_offer"].button = "Make Credit Offer"
+actions["accept_credit_offer"].button = "Accept Credit Offer"
+actions["make_payment"].button = "Make Payment"
+actions["make_reserved_payment"].button = "Make Reserved Payment"
+actions["request_payment"].button = "Request Payment"
+actions["view_credit_balance"].button = "View Credit Balance";
 
 
-actions["make_credit_offer"]["uri"] = function (curr_user, dest_user) {
-    return "/accounts/" + curr_user;
-    };
+actions["make_credit_offer"].uri = function (curr_user, dest_user) {
+    return "/credits/" + curr_user + "/";
+};
+
+actions["accept_credit_offer"].uri = function (curr_user, dest_user) {
+    return "/credits/" + dest_user + "/";
+};
+
+actions["make_payment"].uri = function (curr_user, dest_user) {
+    return "/transactions/" + curr_user + "/";
+};
+
+actions["make_reserved_payment"].uri = function (curr_user, dest_user) {
+    return "/transactions/" + curr_user + "/held/";
+};
+
+actions["request_payment"].uri = function (curr_user, dest_user) {
+    return "/transactions/" + dest_user + "/held/";
+};
+
+actions["view_credit_balance"].uri = function (curr_user, dest_user) {
+    return "/credits/" + curr_user + "/?to=dest_user";
+};
+
+
+actions["make_credit_offer"].meth = "POST"
+actions["accept_credit_offer"].meth = "POST"
+actions["make_payment"].meth = "POST"
+actions["make_reserved_payment"].meth = "POST"
+actions["request_payment"].meth = "POST"
+actions["view_credit_balance"].meth = "GET"
 
